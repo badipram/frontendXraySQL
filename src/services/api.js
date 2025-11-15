6e436b4eb5a796bc23617e17a6da5cde1c75b733
badi amnu <badipram23@outlook.com>
2025-11-15 09:59:53 +0700

    fix API

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,15 @@ export const predictImage = async (file) => {
   const formData = new FormData();
   formData.append('image', file);
 
-  // const response = await fetch('https://backendxray-production.up.railway.app/predict', {
-  //   method: 'POST',
-  //   body: formData,
-  // });
-
-  const response = await fetch('http://127.0.0.1:5050/predict', {
+  const response = await fetch('https://backendsql-production.up.railway.app/predict', {
     method: 'POST',
     body: formData,
   });
+
+  // const response = await fetch('http://127.0.0.1:5050/predict', {
+  //   method: 'POST',
+  //   body: formData,
+  // });
 
   if (!response.ok) {
     const errData = await response.json();
