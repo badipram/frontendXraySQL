279ce644772d4d9784757a7816876118299eb820
badi amnu <badipram23@outlook.com>
2025-11-17 13:12:53 +0700

    fix API4

--- src/services/api.js
+++ src/services/api.js
@@ -7,15 +7,15 @@ export const predictImage = async (file) => {
   //   body: formData,
   // });
 
-  // const response = await fetch('https://backendsql-production.up.railway.app/predict', {
-  //   method: 'POST',
-  //   body: formData,
-  // });
-
-  const response = await fetch('http://127.0.0.1:5050/predict', {
+  const response = await fetch('https://backendsql-production.up.railway.app/predict', {
     method: 'POST',
     body: formData,
   });
+
+  // const response = await fetch('http://127.0.0.1:5050/predict', {
+  //   method: 'POST',
+  //   body: formData,
+  // });
 
   if (!response.ok) {
     const errData = await response.json();
